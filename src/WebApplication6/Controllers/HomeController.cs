9947b2d33dc15df14438a699fed852c4b3855161
Marcin Badurowicz <m@badurowicz.net>
2016-04-26 22:35:25 +0200

    initial revision

---
+++ src/WebApplication6/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Http;
+using System.IO;
+using Microsoft.AspNet.Hosting;
+using WebApplication6.Models;
+
+namespace WebApplication6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IHostingEnvironment _environment;
+
+        public HomeController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+
+        public IActionResult Index()
+        {
+            var uploads = Path.Combine(_environment.WebRootPath, "uploads");
+            var lines = System.IO.File.ReadAllLines(Path.Combine(uploads, "data.txt"));
+
+            SleepData data = SleepData.Parse(lines);
+
+            return View(data);
+        }
+
+        public IActionResult Load()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Load(IFormFile file)
+        {
+            var uploads = Path.Combine(_environment.WebRootPath, "uploads");
+            var fName = "data.txt";
+            file.SaveAs(Path.Combine(uploads, fName));
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/WebApplication6/Models/SleepData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication6.Models
+{
+    public class SleepData
+    {
+        public DateTime Alarm { get; set; }
+        public int Snoozes { get; set; }
+        public List<SleepMovements> Data { get; set; }
+        public DateTime WokeUp { get; set; }
+
+        public static SleepData Parse(string[] lines)
+        {
+            var result = new SleepData();
+            result.Alarm = DateTime.Parse(lines[lines.Length - 4].Split(',')[0]);
+            result.Snoozes = int.Parse(lines[lines.Length - 2].Split(',')[0]);
+
+            result.Data = new List<SleepMovements>();
+
+            for (int i = 0; i < lines.Length -5; i++)
+            {
+                var d = lines[i].Split(',');
+                d[0] = d[0].Replace("<pre>", "");
+
+                if (d[1] == "-1")
+                    break;
+
+                var s = new SleepMovements { Time = d[0], Movements = int.Parse(d[1]) };
+                result.Data.Add(s);
+            }
+
+            return result;
+        }
+    }
+
+    public class SleepMovements
+    {
+        public string Time { get; set; }
+        public int Movements { get; set; }
+    }
+}
